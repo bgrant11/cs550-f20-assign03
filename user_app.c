5486974f85cfeacafb8e34c4a5b68bc5acbeb060
b <bgrant11@binghamton.edu>
2020-12-04 15:50:19 -0500

    long hex not hex

--- user_app.c
+++ user_app.c
@@ -24,7 +24,7 @@ void print_translation(unsigned long va, unsigned long pa){
 		printf("%X ---> NO_FRAME\n", va);
 	} else {
 		printf("%lu ---> %lu\n", va, pa);
-		printf("%X ---> %X\n", va, pa);
+		printf("%lX ---> %lX\n", va, pa);
 	}
 }
 
@@ -37,9 +37,10 @@ int main(int argc, char *argv[]){
 		return -1;
 	}
 	printf("Enter virtual address: ");
-	scanf("%X", &in_addr);
+	scanf("%lX", &in_addr);
 	getchar(); //why is this needed?
-	printf("in_addr: %X\n", in_addr);
+	printf("in_addr: %lu\n", in_addr);	
+	printf("in_addr: %lX\n", in_addr);
 	phys_addr = translate_va(fd, in_addr); 
 	print_translation(in_addr, phys_addr);
 
