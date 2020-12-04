c6fe0bedc738c703dc2c343a23aad280dcec7860
b <bgrant11@binghamton.edu>
2020-12-03 22:55:18 -0500

    iorw to iowr

--- module/page_table.h
+++ module/page_table.h
@@ -3,7 +3,7 @@
 
 #include <linux/ioctl.h>
 
-#define IOCTL_GET_PFN _IORW('p', 1, unsigned long)
+#define IOCTL_GET_PFN _IOWR('p', 1, unsigned long)
 #define MAX_VA 144115188075855871lu
 #define NO_FRAME 18446744073709551615lu
 
