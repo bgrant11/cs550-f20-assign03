c8d2fd00e02c644781ce9aaa91615e5b4b72211a
b <bgrant11@binghamton.edu>
2020-12-04 21:01:21 -0500

    fudged up the offset mask

--- module/page_table.h
+++ module/page_table.h
@@ -6,7 +6,7 @@
 #define IOCTL_GET_PFN _IOWR('p', 1, unsigned long)
 #define MAX_VA 144115188075855871lu
 #define NO_FRAME 18446744073709551615lu
-#define OFFSET_MASK 111111111111
+#define OFFSET_MASK 4095lu	// this is 111111111111 in binary
 #define NUM_OFFSET_BITS 12
 
 #endif
