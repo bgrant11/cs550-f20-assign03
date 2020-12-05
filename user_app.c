6913b69fbe0b00f640834e442e1571901cb40c6a
b <bgrant11@binghamton.edu>
2020-12-04 21:28:52 -0500

    it works, allowing a loop that ends with command: exit

--- user_app.c
+++ user_app.c
@@ -3,6 +3,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <string.h>
+#include <stdlib.h>
 #include <sys/ioctl.h>
 
 
@@ -26,23 +27,60 @@ void print_translation(unsigned long va, unsigned long pa){
 		//printf("%lu ---> %lu\n", va, pa);
 		printf("%lX ---> %lX\n", va, pa);
 	}
+	printf("\n");
+}
+
+void get_input(int fd){
+	unsigned long in_addr;
+	unsigned long phys_addr;
+	char input[100];	
+	char *endpoint;	
+		
+	printf("Enter virtual page number: ");
+	//scanf("%lX", &in_addr);
+	scanf("%s", &input);
+	getchar(); //why is this needed?
+	while(!strcmp(input, "exit"){
+		//printf("in_addr: %lu\n", in_addr);	
+		in_addr = strtoul(input, &endpoint, 16);		
+		printf("in_addr: %lX\n", in_addr);
+		phys_addr = translate_va(fd, in_addr); 
+		print_translation(in_addr, phys_addr);
+	
+		printf("Enter virtual page number: ");
+		//scanf("%lX", &in_addr);
+		scanf("%s", &input);
+		getchar(); //why is this needed?
+	}
+
 }
 
 int main(int argc, char *argv[]){
-	unsigned long in_addr;
-	unsigned long phys_addr;
 	int fd; 
 	if( ( fd = open("/dev/page_table", O_RDWR)) < 0) {
 		printf("Error opening /dev/process_list: %d\n", fd);
 		return -1;
 	}
+	
+	get_input(fd);	
+	/*	
 	printf("Enter virtual page number: ");
-	scanf("%lX", &in_addr);
+	//scanf("%lX", &in_addr);
+	scanf("%s", &input);
 	getchar(); //why is this needed?
-	printf("in_addr: %lu\n", in_addr);	
-	printf("in_addr: %lX\n", in_addr);
-	phys_addr = translate_va(fd, in_addr); 
-	print_translation(in_addr, phys_addr);
-
+	while(!strcmp(input, "exit"){
+		//printf("in_addr: %lu\n", in_addr);	
+		in_addr = strtoul(input, &endpoint, 16);		
+		printf("in_addr: %lX\n", in_addr);
+		phys_addr = translate_va(fd, in_addr); 
+		print_translation(in_addr, phys_addr);
+	
+		printf("Enter virtual page number: ");
+		//scanf("%lX", &in_addr);
+		scanf("%s", &input);
+		getchar(); //why is this needed?
+	}
+	*/
+	close(fd);
 	return 0;
 }
